1826ddf18ff8fed6cd9d2dd41d2afb074a1f82f3
Shadat Rahman <43848931+mdshadatrahman@users.noreply.github.com>
2022-08-25 23:46:25 +0600

    db redone

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ctounter/operations/firebase_crud.dart';
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,12 +17,15 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   FirebaseCrud firebaseCrud = FirebaseCrud();
   int initCount = 0;
-
+  String date = formatDate(DateTime.now(), [dd, mm, yyyy]);
   final player = AudioPlayer();
 
   Future getDataLength() async {
-    QuerySnapshot querySnapshot =
-        await FirebaseFirestore.instance.collection('shakti_counter').get();
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('all_counters')
+        .doc(date)
+        .collection('shakti_counter')
+        .get();
     final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
     setState(() {
       initCount = allData.length;
@@ -33,6 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
     getDataLength();
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -44,63 +49,102 @@ class _HomeScreenState extends State<HomeScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Column(
-                  children: [
-                    StreamBuilder(
-                      stream: FirebaseFirestore.instance
-                          .collection('shakti_counter')
-                          .snapshots(),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          return AutoSizeText(
-                            "${initCount}",
-                            maxLines: 1,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 50,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          );
-                        } else if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return Text("Please Wait");
-                        } else {
-                          return Text("0");
-                        }
-                      },
-                    ),
-                    SizedBox(height: 10),
-                    GestureDetector(
-                      onTap: () async {
-                        firebaseCrud.shaktiCounter();
-                      },
-                      child: Container(
-                        width: 200,
-                        height: 200,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: Colors.red,
-                        ),
-                        child: Center(
-                          child: AutoSizeText(
-                            "Shakti Counter",
-                            maxLines: 1,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                )
+                Counters(
+                  date: date,
+                  initCount: initCount,
+                  firebaseCrud: firebaseCrud,
+                ),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class Counters extends StatelessWidget {
+  const Counters({
+    Key? key,
+    required this.date,
+    required this.initCount,
+    required this.firebaseCrud,
+  }) : super(key: key);
+
+  final String date;
+  final int initCount;
+  final FirebaseCrud firebaseCrud;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('all_counters')
+              .doc(date)
+              .collection('shakti_counter')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return AutoSizeText(
+                "${initCount}",
+                maxLines: 1,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return AutoSizeText(
+                "Please Wait",
+                maxLines: 1,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              );
+            } else {
+              return AutoSizeText(
+                "Something Wrong",
+                maxLines: 1,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              );
+            }
+          },
+        ),
+        SizedBox(height: 10),
+        GestureDetector(
+          onTap: () async {
+            firebaseCrud.shaktiCounter();
+          },
+          child: Container(
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: Colors.red,
+            ),
+            child: Center(
+              child: AutoSizeText(
+                "Shakti Counter",
+                maxLines: 1,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/operations/firebase_crud.dart
+++ lib/operations/firebase_crud.dart
@@ -1,21 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:intl/intl.dart';
+import 'package:date_format/date_format.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
 DateTime now = DateTime.now();
-String onlyDate = DateFormat.yMMMd().format(now);
 
 DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
 
 class FirebaseCrud {
   Future shaktiCounter() async {
-    final _collection =
-        _firestore.collection('shakti_counter').doc(DateTime.now().toString());
     AndroidDeviceInfo info = await deviceInfo.androidInfo;
+
+    DateTime time = DateTime.now();
+    String date = formatDate(time, [dd, mm, yyyy]);
+
+    final _collection = _firestore
+        .collection('all_counters')
+        .doc(date)
+        .collection('shakti_counter')
+        .doc(DateTime.now().toString());
     final data = {
-      onlyDate: {
+      date: {
         "device": info.device,
         "brand": info.brand,
         "manufacturer": info.manufacturer,
@@ -25,21 +31,5 @@ class FirebaseCrud {
       }
     };
     await _collection.set(data);
-  }
-
-  Future<void> sokaleRannaKoreDeycounter() async {
-    final CollectionReference _collection =
-        _firestore.collection('ranna_counter');
-    AndroidDeviceInfo info = await deviceInfo.androidInfo;
-    await _collection.add({
-      onlyDate: {
-        "device": info.device,
-        "brand": info.brand,
-        "manufacturer": info.manufacturer,
-        "model": info.model,
-        "id": info.id,
-        "version": info.version
-      }
-    });
   }
 }
